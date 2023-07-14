3d922f1c21c90c07206ae0f2ce7035b2e6e78666
ChikenSenpai <130470269+ChikenSenpai@users.noreply.github.com>
2023-07-14 11:31:41 +0200

    Add files via upload

---
+++ ShoppingList/ShoppingList/MenuCtrl.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList
+{
+    public static class MenuCtrl
+    {
+        // Add Item:
+        public static ArrayList AddItem(ArrayList list)
+        {
+            string item;
+            bool itemInList;
+            bool itemNotX = true;
+
+            Console.WriteLine("\nEnter the name of the item you want to add and press 'Enter'\nWhen you're done write 'x' and press 'Enter' to stop");
+            do
+            {
+                itemInList = false;
+                item = Console.ReadLine();
+                foreach(string i in list)
+                {
+                    if(i.ToLower() == item.ToLower())
+                    {
+                        Console.WriteLine("The item you're trying to add is already in your list\n");
+                        itemInList = true;
+                    }
+                }
+                if (!itemInList)
+                {
+                    if (item == "")
+                    {
+
+                    }
+                    else if (item.ToLower() == "x")
+                    {
+                        itemNotX = false;
+                    }
+                    else
+                    {
+                        list.Add(item);
+                    }
+                }
+            }while(itemNotX);
+            return list;
+        }
+
+        //Show List:
+        public static void ShowList(ArrayList list)
+        {
+            Console.WriteLine("");
+            if(list.Count > 0)
+            {
+                list.Sort();
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else if(list.Count <= 0)
+            {
+                Console.WriteLine("There are no items in your list yet");
+            }
+            
+        }
+
+        //Remove Item:
+        public static ArrayList RemoveItem(string defaultTxt, ArrayList list) 
+        {
+            string item;
+            bool itemNotX = true;
+            int i;
+
+
+            
+            Console.WriteLine("\nEnter the name of the item you want to remove and press 'Enter'\nWhen you're done write 'x' and press 'Enter' to stop");
+            do
+            {
+                item = Console.ReadLine();
+                if (item == "")
+                {
+
+                }
+                else if (item.ToLower() == "x")
+                {
+                    itemNotX = false;
+                }
+                else
+                {
+                    for (i = 0; i < list.Count; i++)
+                    {
+                        if (list[i].ToString().ToLower() == item.ToLower())
+                        {
+                            Console.WriteLine($"{list[i]} was removed");
+                            list.RemoveAt(i);
+                        }
+                    }
+
+                }
+            } while (itemNotX);
+            return list;
+        }
+
+        //Exit:
+        public static void Exit(string defaultTxt)
+        {
+
+            string choiceExit;
+            bool loopExit = true;
+            bool progExit = false;
+            do
+            {
+                Console.WriteLine("\nAre you sure you want to close the programm?\n\n[1] Yes\n[2] No");
+                choiceExit = Console.ReadLine();
+                switch (choiceExit)
+                {
+                    case "1":
+                        Console.WriteLine("\nClosing ...");
+                        progExit = true;
+                        loopExit = false;
+                        break;
+
+                    case "2":
+                        loopExit = false;
+                        loopExit = false;
+                        break;
+
+                    default:
+                        Console.WriteLine(defaultTxt);
+                        break;
+                }
+            } while (loopExit);
+            if (progExit)
+            {
+                Environment.Exit(0);
+            }
+
+        }
+
+    }
+}
---
+++ ShoppingList/ShoppingList/Program.cs
@@ -0,0 +1,41 @@
+﻿//Libraries:
+using ShoppingList;
+using System.Collections;
+
+//Variables:
+string? menuLiMain;
+string switchDefaultText;
+ArrayList shopList = new ArrayList();
+
+//Declaration:
+switchDefaultText = "Please enter one of the options from above";
+
+do
+{
+    Console.WriteLine("\nShopping List\n----------\n[1] Add Items\n[2] Show List\n[3] Remove Item\n[4] Exit");
+    menuLiMain = Console.ReadLine();
+
+    switch (menuLiMain)
+    {
+        case "1":
+            shopList = MenuCtrl.AddItem(shopList);
+        break;
+
+        case "2":
+            MenuCtrl.ShowList(shopList);
+        break;
+
+        case "3":
+            shopList = MenuCtrl.RemoveItem(switchDefaultText, shopList);
+        break;
+
+        case "4":
+            MenuCtrl.Exit(switchDefaultText);
+        break;
+
+        default:
+            Console.WriteLine($"\n{switchDefaultText}");
+        break;
+
+    }
+} while (true);
